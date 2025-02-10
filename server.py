0642ea2a907504941eaf6036901107ed00734a5e
fjördan <nyanrainbows8@gmail.com>
2025-02-10 15:15:26 -0500

    added timestamps to print statements

--- server.py
+++ server.py
@@ -20,11 +20,10 @@ MAX_CLIENTS = 3     # Limit server to 3 clients
 client_cache = {}   # Cache of all clients that have connected to the server
 active_clients = {} # Dictionary to store active clients
 
-
 # handle client function
 # infinite while loop run on its own thread when a client connects to the server
 # 
-# param client_socket - 
+# param client_socket - the socket used for sending and receiving data from the client
 # param client_name - the name of the client being handled, formatted as Client<xx>
 # param addr - the IP address and port the client is accessing
 def handle_client(client_socket, client_name, addr):
@@ -33,7 +32,7 @@ def handle_client(client_socket, client_name, addr):
     # save client data to our active list and our cache
     active_clients[client_name] = {"start_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
     client_cache[client_name] = active_clients[client_name]
-    print(f"{client_name} connected from {addr}")
+    print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {client_name} connected from {addr}.")
 
     # handling procedures. will run until exit occurs
     while True:
@@ -45,7 +44,7 @@ def handle_client(client_socket, client_name, addr):
             # exit condition
             # save the client's exit date + time to the cache
             if data.lower() == "exit":
-                print(f"{client_name} disconnected.")
+                print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {client_name} disconnected.")
                 client_cache[client_name]["end_time"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 break
 
@@ -53,18 +52,23 @@ def handle_client(client_socket, client_name, addr):
             # send over a list of client information stored in the cache
             elif data.lower() == "status":
                 # Send active clients
+                print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {client_name} requested status.")
                 status = "\n".join(
                     [f"{name}: {info['start_time']} -> {info.get('end_time', 'Active')}" for name, info in client_cache.items()]
                     )
                 client_socket.send(status.encode())
+                print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Sent status to {client_name}.")
 
             # list condition
             # list all files in the server's repository
             elif data.lower() == "list":
                 # Send list of files
+                print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {client_name} requested list.")
                 files = os.listdir("server_files") if os.path.exists("server_files") else []
                 file_list = "\n".join(files) if files else "No files available"
+
                 client_socket.send(file_list.encode())
+                print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Sent list results to {client_name}.")
 
             # get condition
             # transfer a file stored in the repository to the client
@@ -72,18 +76,19 @@ def handle_client(client_socket, client_name, addr):
                 # handshake to confirm that file exists
                 filename = data[4:].strip()
                 directory = f"server_files/{filename}"
+                print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {client_name} requested to get {filename}.")
+
                 if os.path.exists(directory) == False:
                     response = "404 Not Found"              # failure condition
                     client_socket.send(response.encode())
+                    print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {filename} not found.")
                 else:
                     response = "200 OK"                     # success condition
                     client_socket.send(response.encode())
+                    print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {filename} found.")
 
                     # send file to client
-
-                    # test print statement
-                    print(f"Uploading file located at {directory}...")
-
+                    print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Uploading file located at {directory} to {client_name}...")
                     fh = open(directory, "rb")
                     data = fh.read(1024)
                     while data:
@@ -91,15 +96,14 @@ def handle_client(client_socket, client_name, addr):
                         data = fh.read(1024)
                     client_socket.send(b"200 OK")
 
-                    # test print statement
-                    print("Upload complete. ")
-
                     # close file because we love resource management
                     fh.close()
+                    print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Upload complete. ")
 
             else:
                 # Append "ACK" to message and send it back
                 client_socket.send(f"{data} ACK".encode())
+                print(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Echoing {data} sent by {client_name}.")
 
         except Exception as e:
             print(f"Error with client {addr}: {e}")
@@ -126,7 +130,7 @@ def start_server():
             client_name = f"Client{len(client_cache) + 1}"
 
             # complete the handshake for the client
-            handshake = f"Connected to the server as {client_name}. Type 'exit' to disconnect."
+            handshake = f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}]] Connected to the server as {client_name}. "
             client_socket.send(handshake.encode())
 
             # Start a new thread for the client
